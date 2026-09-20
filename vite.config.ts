983c2fc98b30f512f56d55eea5d82fcbaac9cd5e
Sak <sak@users.noreply.github.com>
2026-09-20 19:57:12 +0700

    chore: bump chunk warning limit; add immutable asset + security headers

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     cssCodeSplit: false,
+    chunkSizeWarningLimit: 650,
     rollupOptions: {
       output: {
         entryFileNames: "assets/app.js",
